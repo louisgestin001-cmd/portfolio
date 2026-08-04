824d62468308f3d2d48f733923356520dda63006
louis gestin <louisgestin001@gmail.com>
2026-08-04 23:43:32 +0200

    Add NG-StateMin package exports

---
+++ projects/ng-statemin-u1/src/ng_statemin/__init__.py
@@ -0,0 +1,6 @@
+"""NG-StateMin-U1: a minimal piecewise-linear recurrent state law."""
+
+from .cells import NGStateMinU1Cell, NGStateMinU1Layer, ng_state_min_u1
+
+__all__ = ["NGStateMinU1Cell", "NGStateMinU1Layer", "ng_state_min_u1"]
+__version__ = "0.1.0"
