51abf14ea570c35ef58783846d2c38dc69d06981
louis gestin <louisgestin001@gmail.com>
2026-08-05 12:21:58 +0200

    Export NG-TriSpecies public API

--- projects/ng-cell-lab/src/ng_cell_lab/__init__.py
+++ projects/ng-cell-lab/src/ng_cell_lab/__init__.py
@@ -8,6 +8,7 @@ from .cells import (
     NGShiftCompareMul,
     NGStateMinU1,
 )
+from .mlp import NGTriSpeciesMLP, allocate_species_sizes, signed_log1p
 from .registry import CELL_CARDS, CellCard, make_cell
 from .sequence import NGSequenceLayer
 
@@ -18,8 +19,11 @@ __all__ = [
     "NGEnergyMax1",
     "NGEnergyMax2",
     "NGLagMean1",
+    "NGTriSpeciesMLP",
     "NGSequenceLayer",
     "NGShiftCompareMul",
     "NGStateMinU1",
+    "allocate_species_sizes",
     "make_cell",
+    "signed_log1p",
 ]
