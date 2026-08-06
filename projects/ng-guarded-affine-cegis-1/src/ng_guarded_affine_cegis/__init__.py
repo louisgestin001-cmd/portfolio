68ca406f3a530cc2d1d3b239e3a02df97fdf7983
louis gestin <louisgestin001@gmail.com>
2026-08-06 22:30:54 +0200

    Add NG-GuardedAffine package exports

---
+++ projects/ng-guarded-affine-cegis-1/src/ng_guarded_affine_cegis/__init__.py
@@ -0,0 +1,4 @@
+from .compiler import CEGISCertificate, GuardedAffineCEGIS
+from .model import AffineMap, Coordinate, GuardNode, GuardedAffineProgram
+from .protocol import DeterministicNoisyOracle, ReliableCreditProtocol
+__all__=["AffineMap","CEGISCertificate","Coordinate","DeterministicNoisyOracle","GuardNode","GuardedAffineCEGIS","GuardedAffineProgram","ReliableCreditProtocol"]
